c2e50f15b4d2381fc70b98d566d3890fe2992504
Darshu9945 <120715519+Darshu9945@users.noreply.github.com>
2023-07-25 13:18:44 +0530

    added from

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
 import React from 'react';
+import Myform from './components/forms/Myform';
 
 function App() {
   return (
     <React.Fragment>
     <div >
+      <Myform></Myform>
       <Button></Button>
       </div>
       
---
+++ src/components/forms/Myform.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './myform.css'
+import { useState } from 'react'
+const Myform = () => {
+const [title,settitle]=useState('')
+const [desciption,setdesciption]=useState('')
+const [date,setdate]=useState('')
+const titlehandler=(e)=>{
+settitle(e.target.value)
+}
+const descriptionhandler=(e)=>{
+    setdesciption(e.target.value)
+}
+const datehandler=(e)=>{
+    setdate(e.target.value)
+}
+const submithandler=()=>{
+    const obj={
+        title:title,
+        desciption:desciption,
+        date:date
+    }
+    console.log(obj)
+}
+
+  return (
+    <div className='myform'>
+        <form onSubmit={submithandler}>
+      <label>Title</label><br></br>
+      <input onChange={titlehandler} type="text" /><br></br>
+      <label >Description </label><br></br>
+      <textarea onChange={descriptionhandler} className='description' rows="7" cols="50" type="text" /><br></br>
+      <label>Release date </label><br></br>
+      <input onChange={datehandler} type="text" /><br></br>
+      <button type='submit'>Add Movie</button>
+      </form>
+    </div>
+  )
+}
+
+export default Myform
